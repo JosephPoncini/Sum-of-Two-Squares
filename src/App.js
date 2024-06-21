8267548c00429b34edbc6f238cf76bd09caf3507
JosephPoncini <jxponcini@gmail.com>
2024-06-21 08:27:00 -0700

    shortened the range to a billion because I was finding errors when doing prime numbers above that

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     const value = e.target.value;
 
     // Check if the value is an integer
-    if (/^\d+$/.test(value) && value.length < 13) {
+    if (/^\d+$/.test(value) && parseInt(value, 10) < 1000000000) {
       setNumber(parseInt(value, 10));
     } else if (value === '') {
       setNumber(0);
@@ -37,7 +37,7 @@ function App() {
       <h1 className="text-3xl font-ArchivoBlack">
         SUM OF TWO SQUARES CALCULATOR
       </h1>
-      <h6 className=' font-RobotoSerif '> Have you ever come by a number and wondered... can this number be written as the sum of two perfect square?? If so, can it be written more than one way like? If you have had these thoughts, it turns out you are not alone. Mathematician in the last 300 years have had similar quandries and have been able to come up with some relatively fast algorithms to answer these questions. In the calculator below, I have integrated some of these algorithms to calculate for a given number (12 digits or fewer) all possible squares that will sum up to it. If you are curious as to how I developed my code, I heavily leveraged knowledge gained from this site: <a href='https://nonagon.org/ExLibris/fermat-sum-two-squares-calculator' target='_blank' className=' underline font-bold text-blue-900'>https://nonagon.org/ExLibris/fermat-sum-two-squares-calculator</a>. Furthermore, if you would like to try coding this yourself, you may use my <a className=' underline font-bold text-blue-900' href='https://github.com/JosephPoncini/Sum-of-Two-Squares' target='_blank'>github</a>  for help as well.</h6>
+      <h6 className=' font-RobotoSerif '> Have you ever come by a number and wondered... can this number be written as the sum of two perfect square?? If so, can it be written more than one way like? If you have had these thoughts, it turns out you are not alone. Mathematician in the last 300 years have had similar quandries and have been able to come up with some relatively fast algorithms to answer these questions. In the calculator below, I have integrated some of these algorithms to calculate for a given number, up to a billion, all possible squares that will sum up to it. If you are curious as to how I developed my code, I heavily leveraged knowledge gained from this site: <a href='https://nonagon.org/ExLibris/fermat-sum-two-squares-calculator' target='_blank' className=' underline font-bold text-blue-900'>https://nonagon.org/ExLibris/fermat-sum-two-squares-calculator</a>. Furthermore, if you would like to try coding this yourself, you may use my <a className=' underline font-bold text-blue-900' href='https://github.com/JosephPoncini/Sum-of-Two-Squares' target='_blank'>github</a>  for help as well.</h6>
 
       <div className=' bg-white py-10 md:h-[300px] w-[300px] md:w-[500px] rounded-md border-2 border-black flex flex-col items-center justify-center space-y-5 px-10 '>
         <div className=' font-RobotoSerif text-center' >Enter in the number you want to know the sum of two squares for:</div>
